4138d985f324d473640c990134dedfde6446bf47
progr <programmist00707@gmail.com>
2022-08-11 20:19:33 +0600

    Professions

---
+++ src/Persons/Lesson14.java
@@ -0,0 +1,14 @@
+package Persons;
+
+public class Lesson14 {
+    public static void main(String[] args) {
+        Programmer programmer = new Programmer("Syimyk", "Coding", "PeakSoft");
+        Dancer dancer = new Dancer("Mahamatzhan", "Dancing", "Best Dancers");
+        Singer singer = new Singer("Argen", "Singing", "AC & DC");
+
+        System.out.println(programmer);
+        System.out.println(dancer);
+        System.out.println(singer);
+
+    }
+}
