cc3aa634b1f83fb3da58ad17f20e9e5fdea9f40a
progr <programmist00707@gmail.com>
2022-08-12 19:32:51 +0600

    Final short code

--- src/SortingStudent/Main.java
+++ src/SortingStudent/Main.java
@@ -3,6 +3,7 @@ package SortingStudent;
 import java.util.Random;
 import java.util.Scanner;
 public class Main {
+    /*Syimyk, Argen Iraliev, Ayperi, Bakai, Burulai, Kerezbek, Milana, Mirgul, Nuraiym, Nurislam Nurkanov, Nurlan, Nursultan Osorov, Oroz, Samarbek, Zhazgul, Nursultan Zhumataev, Azimbek, Aidana, Baisalbek, Argen Abdymomunov, Jumgalbek, Kasiet, Mahamatjan, Kairat, Nurislam, Nurkyz, Nurtilek, Eldiar, Aktan, Erjan, Aibek, Iskender, Ruslan, Ramazan, Bektemir, Narboto, Ali, Nurjigit, Altylbek, Kylym, Azamat*/
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String names = scanner.nextLine();
