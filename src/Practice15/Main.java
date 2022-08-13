eeb01bc5b5ba9d07cdd019d3c79419fa7c359c7e
progr <programmist00707@gmail.com>
2022-08-13 14:01:01 +0600

    HomeWork 15 lesson

---
+++ src/Practice15/Main.java
@@ -0,0 +1,12 @@
+package Practice15;
+
+public class Main {
+    public static void main(String[] args) {
+        Cirle cirle = new Cirle();
+
+        cirle.setPI(15);
+        cirle.area(15, 15);
+        System.out.println();
+        cirle.circumference(45);
+    }
+}
